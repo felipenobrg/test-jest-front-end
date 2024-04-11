0ac4be63308b72258fb4a3f8e064a78491740ef7
Felipe Nóbrega de Almeida <felipenobrega2012@gmail.com>
2024-04-11 10:21:55 -0300

    test: testing transferencia function

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,3 +1,29 @@
+import { calculaNovoSaldo} from './index'
+
+describe('Quando eu realizo uma transação', () => {
+    test("Que é um depósito, o saldo deve aumentar", () => {
+        const transacao = {
+            transacao: 'Depósito',
+            valor: 50,
+        }
+        const novoSaldo = calculaNovoSaldo(transacao, 100)
+
+        expect(novoSaldo).toBe(150)
+    })
+})
+
+describe('Quando eu realizo uma transação', () => {
+    test("Que é um transferência, o saldo deve diminuir", () => {
+        const transacao = {
+            transacao: 'Trasferência',
+            valor: 50,
+        }
+        const novoSaldo = calculaNovoSaldo(transacao, 100)
+
+        expect(novoSaldo).toBe(50)
+    })
+})
+
 test('Deve retorna o valor do saldo atualizado com o rendimento', () => {
     const calculaRendimento = jest.fn(saldo => saldo + saldo * 0.005)
 
